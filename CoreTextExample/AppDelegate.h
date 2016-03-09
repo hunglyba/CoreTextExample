7119d718edf7c577d5f407e61556e13d4e603bfb
hunglyba <tvo001@tvo001s-imac.local>
2016-03-09 13:44:13 +0700

    Initial Commit

---
+++ CoreTextExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextExample
+//
+//  Created by Tvo001 on 3/9/16.
+//  Copyright © 2016 Tvo001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
